00bbd2b2805c9f0540f447f168e3608c1e99c1d5
A Yeong Jeon <AYeong@DESKTOP-TJ9F5CL>
2022-10-06 21:15:46 +0900

    06

---
+++ 02_Test/src/main/java/October/Secret_map.java
@@ -0,0 +1,11 @@
+package October;
+
+public class Secret_map {
+	
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        
+        
+        return answer;
+    }
+}
