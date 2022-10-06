1088c8bf6377ef1d0adfc6e01b9a605b90cdbca7
A Yeong Jeon <AYeong@DESKTOP-TJ9F5CL>
2022-10-07 01:12:38 +0900

    SecretMap

--- 02_Test/src/main/java/October/Secret_map.java
+++ 02_Test/src/main/java/October/Secret_map.java
@@ -1,11 +1,22 @@
 package October;
 
 public class Secret_map {
-	
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
-        
+    public static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        	
+	        for(int i=0; i<n; i++) {
+	        	answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+	        	
+	        	answer[i] = String.format("%"+n+"s", answer[i]);
+	        	
+	        	answer[i] = answer[i].replaceAll("1", "#");
+	        	answer[i] = answer[i].replaceAll("0", " ");
+	        	
+	        	answer[i] = answer[i];
+	        }
         
         return answer;
     }
 }
+
+
